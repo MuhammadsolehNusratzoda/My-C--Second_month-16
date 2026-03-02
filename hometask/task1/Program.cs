204c50cd1e030ec4b1e447556b5ca26f88dc4fe1
Muhammadsoleh <nusratzodasuhaib@gmail.com>
2026-03-02 13:41:23 +0500

    chnges

--- hometask/task1/Program.cs
+++ hometask/task1/Program.cs
@@ -1,7 +1,7 @@
 ﻿
 using U;
 
-        Cache<int>.Add(10);
+        Cache<int>.Add(7);
         Cache<int>.Add(11);
         Cache<string>.Add("Ronaldo");
         Cache<string>.Add("Messi");
--- hometask/task1/obj/Debug/net9.0/task1.AssemblyInfo.cs
+++ hometask/task1/obj/Debug/net9.0/task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e1de2513c748e9a90f60c7a3166b67276c2e47d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84eaf3407c84c41706fccb49293e470495c0ecae")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
