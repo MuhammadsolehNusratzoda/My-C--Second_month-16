f782457f17b18f044b464e7dfa8393d023ee3963
Muhammadsoleh <nusratzodasuhaib@gmail.com>
2026-03-02 13:43:45 +0500

    chnges

--- hometask/task1/Program.cs
+++ hometask/task1/Program.cs
@@ -1,23 +1,23 @@
 ﻿
 using U;
 
-        Cache<int>.Add(7);
-        Cache<int>.Add(11);
-        Cache<string>.Add("Ronaldo");
-        Cache<string>.Add("Messi");
+Cache<int>.Add(7);
+Cache<int>.Add(11);
+Cache<string>.Add("Ronaldo");
+Cache<string>.Add("Messi");
 
-        var s2 = Cache<string>.Get();
+var s2 = Cache<string>.Get();
 
-        foreach (var item in s2)
-        {
-            Console.WriteLine(item);
-        }
+foreach (var item in s2)
+{
+    Console.WriteLine(item);
+}
 
-        Cache<int>.Remove(10);
+Cache<int>.Remove(10);
 
-        var s = Cache<int>.Get();
+var s = Cache<int>.Get();
 
-        foreach (var item in s)
-        {
-            Console.WriteLine(item);
-        }
+foreach (var item in s)
+{
+    Console.WriteLine(item);
+}
